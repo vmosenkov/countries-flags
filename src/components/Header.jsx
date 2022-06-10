a7474e7fee11701871d2e94461178ec1e4624753
vmosenkov <vladmos1995@gmail.com>
2022-06-10 11:29:59 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { IoMoonOutline, IoMoon } from "react-icons/io5";
+
+import { Container } from './Container';
+
+const HeaderEl = styled.header``;
+
+const Header = () => {
+  return (
+    <HeaderEl>
+     <Container>
+     
+     </Container>
+    </HeaderEl>
+  )
+}
+
+export default Header
