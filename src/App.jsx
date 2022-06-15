4683b6f7953f9416db95aae188749941f40f7e9d
vmosenkov <vladmos1995@gmail.com>
2022-06-15 12:10:14 +0300

    proj complete

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+/* Components */
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
+
+/* Pages */
+import { HomePage } from './pages/HomePage';
+import { Details } from './pages/Details';
+import { NotFound } from './pages/NotFound';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  return (
+    <>
+      <Header />
+      <Main>
+        <Switch>
+          <Route exact path="/">
+            <HomePage countries={countries} setCountries={setCountries} />
+          </Route>
+          <Route path="/country/:name" component={Details} />
+          <Route component={NotFound} />
+        </Switch>
+      </Main>
+    </>
+  );
+}
+
+export default App;
